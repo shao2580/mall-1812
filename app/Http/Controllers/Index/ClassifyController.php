40a1fc9389c6057763ee7fa717723fb0981af40a
1153666747 <root@localhost.localdomain>
2019-09-19 15:31:43 +0800

    前台模板搭建

---
+++ app/Http/Controllers/Index/ClassifyController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers\Index;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+class ClassifyController extends Controller
+{
+
+//分类页面
+public function classify()
+{
+    return view('index.classify');
+}
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,35 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//登录页面
+Route::prefix('/index')->group(function(){
+    route::any('login','Index\LoginController@login');//登录页面
+    route::any('register','Index\LoginController@register');//注册页面
+});
+
+//前台首页
+Route::prefix('/index')->group(function(){
+    route::any('index','Index\IndexController@index');
+});
+
+//分类页面
+Route::prefix('/index')->group(function(){
+    route::any('classify','Index\ClassifyController@classify');
+});
+
+//商品详情
+Route::prefix('/index')->group(function(){
+    route::any('goods','Index\GoodsController@goods');
+});
+
+//购物车页面
+Route::prefix('/index')->group(function(){
+    route::any('cart','Index\CartController@cart');
+});
+
+//个人中心
+Route::prefix('/index')->group(function(){
+    route::any('center','Index\CenterController@center');//我的信息
+    route::any('order','Index\CenterController@order');//我的订单
+});
