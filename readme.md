5a122bd463232c7d4f6154e552af75dea579405d
shao2580 <shao2580@localhost.localdomain>
2019-09-23 06:00:57 +0000

    shao-dev 修改bug+分配

--- readme.md
+++ readme.md
@@ -1,72 +1,45 @@
-<p align="center"><img src="https://res.cloudinary.com/dtfbvvkyp/image/upload/v1566331377/laravel-logolockup-cmyk-red.svg" width="400"></p>
+### mall-1812   购物中心  B2C
 
-<p align="center">
-<a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/d/total.svg" alt="Total Downloads"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/v/stable.svg" alt="Latest Stable Version"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/license.svg" alt="License"></a>
-</p>
+**前台功能：**
+​	用户登录、注册   ----田胤辰
 
-## About Laravel
+个人中心：
+​	- 个人资料：信息、安全设置、收货地址    ---张腾（向下做↓）
 
-Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
+​	- 我的交易：订单管理：（所有订单、待付款、待发货、待收货、待评价）
+​		  退款售后
 
-- [Simple, fast routing engine](https://laravel.com/docs/routing).
-- [Powerful dependency injection container](https://laravel.com/docs/container).
-- Multiple back-ends for [session](https://laravel.com/docs/session) and [cache](https://laravel.com/docs/cache) storage.
-- Expressive, intuitive [database ORM](https://laravel.com/docs/eloquent).
-- Database agnostic [schema migrations](https://laravel.com/docs/migrations).
-- [Robust background job processing](https://laravel.com/docs/queues).
-- [Real-time event broadcasting](https://laravel.com/docs/broadcasting).
+​	- 我的资产：优惠券、红包、账单明细	
 
-Laravel is accessible, powerful, and provides tools required for large, robust applications.
+​	- 我的小窝：收藏、足迹、详情、信息     ----杨绍峰 （向上↑）
 
-## Learning Laravel
+分类：无限极   ----张腾（在做...）
 
-Laravel has the most extensive and thorough [documentation](https://laravel.com/docs) and video tutorial library of all modern web application frameworks, making it a breeze to get started with the framework.
+购物车管理:
+​	首页商品展示、加入购物车   ----赵子龙 ​	
 
-If you don't feel like reading, [Laracasts](https://laracasts.com) can help. Laracasts contains over 1500 video tutorials on a range of topics including Laravel, modern PHP, unit testing, and JavaScript. Boost your skills by digging into our comprehensive video library.
+支付|物流		
 
-## Laravel Sponsors
 
-We would like to extend our thanks to the following sponsors for funding Laravel development. If you are interested in becoming a sponsor, please visit the Laravel [Patreon page](https://patreon.com/taylorotwell).
 
-- **[Vehikl](https://vehikl.com/)**
-- **[Tighten Co.](https://tighten.co)**
-- **[Kirschbaum Development Group](https://kirschbaumdevelopment.com)**
-- **[64 Robots](https://64robots.com)**
-- **[Cubet Techno Labs](https://cubettech.com)**
-- **[Cyber-Duck](https://cyber-duck.co.uk)**
-- **[British Software Development](https://www.britishsoftware.co)**
-- **[Webdock, Fast VPS Hosting](https://www.webdock.io/en)**
-- **[DevSquad](https://devsquad.com)**
-- [UserInsights](https://userinsights.com)
-- [Fragrantica](https://www.fragrantica.com)
-- [SOFTonSOFA](https://softonsofa.com/)
-- [User10](https://user10.com)
-- [Soumettre.fr](https://soumettre.fr/)
-- [CodeBrisk](https://codebrisk.com)
-- [1Forge](https://1forge.com)
-- [TECPRESSO](https://tecpresso.co.jp/)
-- [Runtime Converter](http://runtimeconverter.com/)
-- [WebL'Agence](https://weblagence.com/)
-- [Invoice Ninja](https://www.invoiceninja.com)
-- [iMi digital](https://www.imi-digital.de/)
-- [Earthlink](https://www.earthlink.ro/)
-- [Steadfast Collective](https://steadfastcollective.com/)
-- [We Are The Robots Inc.](https://watr.mx/)
-- [Understand.io](https://www.understand.io/)
-- [Abdel Elrafa](https://abdelelrafa.com)
-- [Hyper Host](https://hyper.host)
+**后台功能：**
+​	用户管理：
+​		列表	----田胤辰（正在做.....）
 
-## Contributing
+商品管理：
+​	添加商品：张珊  
+​		- 商品基本信息：名称、货号、分类、品牌、售价、（送积分）
+​		- 详细描述：图片+文字+排版   超文本域
+​		- 商品属性：
+​			sku-各种型号库存  添加	（待定）					
 
-Thank you for considering contributing to the Laravel framework! The contribution guide can be found in the [Laravel documentation](https://laravel.com/docs/contributions).
+​	商品类型管理	     张珊  （在做..）
+​	商品列表：
 
-## Security Vulnerabilities
+​	品牌管理		于洋宽	
 
-If you discover a security vulnerability within Laravel, please send an e-mail to Taylor Otwell via [taylor@laravel.com](mailto:taylor@laravel.com). All security vulnerabilities will be promptly addressed.
+商品分类：无限极   ----张腾
 
-## License
-
-The Laravel framework is open-source software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+订单管理：
+​	订单查询  	闫辉（在做...）
+​	修改订单状态
--- routes/web.php
+++ routes/web.php
@@ -18,88 +18,74 @@ Route::get('/', function () {
  
 /*后台*/
 Route::prefix('/admin')->middleware([])->group(function(){
-    Route::any('index','Admin\IndexController@index');		//首页
-    Route::any('welcome','Admin\IndexController@welcome');  //首页图
+    Route::get('index','Admin\IndexController@index');		//首页
+    Route::get('welcome','Admin\IndexController@welcome');  //首页图
 
-    Route::any('list','Admin\IndexController@list');		//管理员列表
-    Route::any('add','Admin\IndexController@add');          //管理员添加
+    Route::get('list','Admin\IndexController@list');		//管理员列表
+    Route::post('add','Admin\IndexController@add');          //管理员添加
 });
 
 Route::prefix('/vip')->middleware([])->group(function(){
-    Route::any('vlist','Admin\UserController@vlist');     //会员列表
+    Route::get('vlist','Admin\UserController@vlist');     //会员列表
 });
 
 Route::prefix('/login')->group(function(){
-    Route::any('login','Admin\LoginController@login');         //登录
+    Route::get('login','Admin\LoginController@login');         //登录
     Route::any('logindo','Admin\LoginController@loginDo');     //执行登录
-    Route::any('loginout','Admin\LoginController@loginOut');   //退出登录
-});
-//商品
-Route::prefix('/goods')->middleware([])->group(function(){
-    Route::any('list','Admin\GoodsController@list');     //商品列表
-    Route::any('add','Admin\GoodsController@add');     //商品添加
-    Route::any('doAdd','Admin\GoodsController@doAdd');     //执行添加
-    Route::any('upload','Admin\GoodsController@upload');     //执行添加
+    Route::post('loginout','Admin\LoginController@loginOut');   //退出登录
 });
 
+//商品
+Route::prefix('/goods')->middleware([])->group(function(){
+    Route::get('list','Admin\GoodsController@list');     //商品列表
+    Route::get('add','Admin\GoodsController@add');     //商品添加
+    Route::post('doAdd','Admin\GoodsController@doAdd');     //执行添加
+    Route::any('upload','Admin\GoodsController@upload');     //执行添加
+});
 
 //分类管理
 Route::prefix('/category')->group(function(){
     Route::any('create','Admin\CategoryController@create');//分类添加页面
     Route::any('save','Admin\CategoryController@save');     //添加处理页面
     Route::any('list','Admin\CategoryController@list');     //列表展示页面
-    // Route::any('loginout','Admin\LoginController@loginOut');   //退出登录
 });
 
-/*前台*/
-Route::prefix('index')->group(function(){    
-    //不用登陆
-    Route::namespace('Index')->group(function(){  
-        Route::any('index','IndexController@index');             //主页
-    });
+#专栏管理
+Route::prefix('/fenlan')->group(function(){
+    Route::get('add','FenlanController@add');//添加视图
+    Route::post('doadd','FenlanController@doadd');//执行添加
+    Route::get('lists','FenlanController@lists');//展示
+    Route::post('del','FenlanController@del');//删除
+    Route::get('update','FenlanController@update');//修改
+    Route::post('doupdate','FenlanController@doupdate');//执行修改
+});
 
-    //登陆 注册
-    Route::middleware([])->namespace('Index')->group(function(){   
-         Route::any('login','IndexController@login');                 //登录
-         Route::any('register_1','IndexController@registerV1');       //注册1
-         Route::any('register_2','IndexController@registerV2');       //注册2
-    });
 
-    //登陆
-    Route::middleware([])->namespace('Index')->group(function(){   
-        Route::any('userinfo','UserController@userinfo');   //个人资料
-        Route::any('password','UserController@password');   //修改密码
-        Route::any('email','UserController@email');         //修改邮箱
-    }); 
-}); 
+/*前台首页*/
+Route::prefix('/index')->group(function(){
+    route::get('index','Index\IndexController@index');
+});
 
  //登录页面
 Route::prefix('/index')->group(function(){
-    route::any('login','Index\LoginController@login');//登录页面
-    route::any('loginDo','Index\LoginController@loginDo');//登录执行页面
-    route::any('loginout','Index\LoginController@loginout');//退出
+    route::get('login','Index\LoginController@login');//登录页面
+    route::post('loginDo','Index\LoginController@loginDo');//登录执行页面
+    route::get('loginout','Index\LoginController@loginout');//退出
 
-    route::any('register','Index\LoginController@register');//注册页面
-    route::any('registerDo','Index\LoginController@registerDo');//注册执行页面
-    route::any('checkphone','Index\LoginController@checkphone');//检查手机号是否存在
-    route::any('send','Index\LoginController@send');//检查手机号是否存在
-
-
-});
-
-//前台首页
-Route::prefix('/index')->group(function(){
-    route::any('index','Index\IndexController@index');
+    route::get('register','Index\LoginController@register');//注册页面
+    route::post('registerDo','Index\LoginController@registerDo');//注册执行页面
+    route::post('checkphone','Index\LoginController@checkphone');//检查手机号是否存在
+    route::any('send','Index\LoginController@send');//发送验证码
 });
 
 //分类页面
 Route::prefix('/index')->group(function(){
-    route::any('classify','Index\ClassifyController@classify');
+    route::get('classify','Index\ClassifyController@classify');
 });
 
 //商品详情
 Route::prefix('/index')->group(function(){
-    route::any('goods/{goods_id}','Index\GoodsController@goods');
+    route::get('goods/{goods_id}','Index\GoodsController@goods');
     //添加购物车
     route::post('addcart','Index\GoodsController@addcart');
 });
@@ -115,21 +101,13 @@ Route::prefix('/index')->group(function(){
 
 //个人中心
 Route::prefix('/index')->group(function(){
-    route::any('center','Index\CenterController@center');//我的信息
-    route::any('order','Index\CenterController@order');//我的订单
-    route::any('site','Index\CenterController@site');//我的地址
-    route::any('siteSave','Index\CenterController@siteSave');//我的地址 省 市 区
-    route::any('create','Index\CenterController@create');//我的地址 添加处理页面
+    route::get('center','Index\CenterController@center');//我的信息
+    route::get('order','Index\CenterController@order');//我的订单
+    route::get('site','Index\CenterController@site');//我的地址
+    route::get('siteSave','Index\CenterController@siteSave');//我的地址 省 市 区
+    route::get('create','Index\CenterController@create');//我的地址 添加处理页面
 });
 
 
-#专栏管理
-Route::prefix('/fenlan')->group(function(){
-    Route::get('add','FenlanController@add');//添加视图
-    Route::any('doadd','FenlanController@doadd');//执行添加
-    Route::any('lists','FenlanController@lists');//展示
-    Route::any('del','FenlanController@del');//删除
-    Route::any('update','FenlanController@update');//修改
-    Route::any('doupdate','FenlanController@doupdate');//执行修改
-});
+
 
